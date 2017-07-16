a52d1381d618a9b705468ed75a58051642cee1de
Emil Trebicka <etrebicka@remedia.it>
2017-07-16 10:54:26 +0200

    layout fix

--- Web/ClientApp/components/Layout.tsx
+++ Web/ClientApp/components/Layout.tsx
@@ -13,10 +13,10 @@ export class Layout extends React.Component<LayoutProps, void> {
             <div className='container-fluid'>
                 {!(currentPath == '/') ?
                     <div className='row'>
-                        <div className='col-sm-3'>
+                        <div className='col-sm-2'>
                             <NavMenu />
                         </div>
-                        <div className='col-sm-9'>
+                        <div className='col-sm-10'>
                             {this.props.body}
                         </div>
                     </div>
